5c68de6ba89b7c0fca91d3ca42977c702f70c6ac
Thimira Vimukthi <ictthimira@gmail.com>
2025-03-01 09:11:12 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,7 +13,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/vimu98",
   resume:
-    "https://drive.google.com/file/d/1IwcfmH_3YpxeCZnJH0JomnVizDUUOn6t/view?usp=sharing",
+    "https://drive.google.com/file/d/1EcIU3irndGOd_VHhoaXUdpoUfI9cAgin/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/vimu98/",
   twitter: "https://www.youtube.com/@VimukthiOfficial/",
   insta: "https://www.instagram.com/thimira.v/",
